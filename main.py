418bcb6ecd524365cbe630a66e15c4f35b7509cc
Rich Desktop <richard.bay@hotmail.co.uk>
2020-03-05 18:32:54 +0000

    Spyder
    Spyder bits now here - realised NFL does not have all the stats I want - may have to restart whole process with fantasy pros stats.

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+#Set Headings
+QB_headings = ["WK", "Game Date", "Opp", "Result", "G", "GS", "P-Comp", "P-Att", "P-Pct", "P-Yds", "P-Avg", "P-TD", "Int", "Sck", "SckY", "Rate", "R-Att", "R-Yds", "R-Avg", "R-TD", "FUM", "Lost"]
+RB_headings = ["WK", "Game Date", "Opp", "Result", "G", "GS", "R-Att", "R-Yds", "R-Avg", "R-Lng", "R-TD", "Rec", "Re-Yds", "Re-Avg", "Re-Lng", "FUM", "Lost"]
+WR_headings = ["WK", "Game Date", "Opp", "Result", "G", "GS", "P-Comp", "P-Att", "P-Pct", "P-Yds", "P-Avg", "P-TD", "Int", "Sck", "SckY", "Rate", "R-Att", "R-Yds", "R-Avg", "R-TD", "FUM", "Lost"]
+TE_headings = ["WK", "Game Date", "Opp", "Result", "G", "GS", "P-Comp", "P-Att", "P-Pct", "P-Yds", "P-Avg", "P-TD", "Int", "Sck", "SckY", "Rate", "R-Att", "R-Yds", "R-Avg", "R-TD", "FUM", "Lost"]
+
 QB_raw = pd.read_csv("QBout.csv").set_index("Player")
 
-player_list = 
+#player_list =
 
 #Make the link column the complete link
 QB_raw["Link"] = ["http://www.nfl.com/"+i+"gamelogs?season=2019" for i in QB_raw["Link"].tolist()]
 
-page = requests.get(QB_raw.loc["Allen, Brandon", "Link"])
+#page = requests.get(QB_raw.loc["Allen, Brandon", "Link"])
 
-soup = BeautifulSoup(page.content, 'html.parser')
+#soup = BeautifulSoup(page.content, 'html.parser')
 
-headings = [heading.get_text() for heading in soup.find("tr", class_ = "player-table-key").find_all("td")]
-print(headings)
+#headings = [heading.get_text() for heading in soup.find("tr", class_ = "player-table-key").find_all("td")]
 
-#for output in outputs.find_all("td"):
-  #print(output)
